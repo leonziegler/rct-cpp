7913e00a0ff3d355b8dd12879429b7bbf51ed1e1
Leon Ziegler <lziegler@techfak.uni-bielefeld.de>
2015-03-06 16:48:53 +0100

    added collection header

---
+++ core/src/rct/rct.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <rct/rctConfig.h>
+#include <rct/Transform.h>
+#include <rct/Transformer.h>
+#include <rct/TransformerConfig.h>
+#include <rct/TransformerFactory.h>
+#include <rct/impl/TransformerCore.h>
